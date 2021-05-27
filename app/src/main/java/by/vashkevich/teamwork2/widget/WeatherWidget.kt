1ddd7642697e3327c27c726b722488a994e15ef7
DESKTOP-NN2F9VB\Yume <enchanteur@mail.ru>
2021-05-27 15:39:09 +0300

    Branch_3

--- app/src/main/java/by/vashkevich/teamwork2/widget/WeatherWidget.kt
+++ app/src/main/java/by/vashkevich/teamwork2/widget/WeatherWidget.kt
@@ -1,8 +1,11 @@
 package by.vashkevich.teamwork2.widget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.util.Log
 import android.widget.RemoteViews
@@ -23,10 +26,15 @@ import java.util.*
  * App Widget Configuration implemented in [WeatherWidgetConfigureActivity]
  */
 class WeatherWidget : AppWidgetProvider() {
+
+    companion object {
+        const val TAG = "tag"
+    }
+
     override fun onUpdate(
-            context: Context,
-            appWidgetManager: AppWidgetManager,
-            appWidgetIds: IntArray
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
     ) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
@@ -52,9 +60,9 @@ class WeatherWidget : AppWidgetProvider() {
 }
 
 internal fun updateAppWidget(
-        context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetId: Int
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
 ) {
     val lat: Double = loadLatitude(context, appWidgetId).toDouble()
     val lon: Double = loadLongitude(context, appWidgetId).toDouble()
@@ -63,11 +71,11 @@ internal fun updateAppWidget(
 }
 
 private fun load(
-        lat: Double,
-        lon: Double,
-        context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetId: Int
+    lat: Double,
+    lon: Double,
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
 ) {
     //классика .репозиторий .скоуп
     val repository = WeatherRepository.getRepository()
@@ -82,15 +90,21 @@ private fun load(
         withContext(Dispatchers.Main) {
             // Construct the RemoteViews object
 
-
             val views = RemoteViews(context.packageName, R.layout.weather_widget)
 
             val arrayData = listOf<Int>(R.id.data1, R.id.data2, R.id.data3, R.id.data4)
             val arrayIcon = listOf<Int>(R.id.icon1, R.id.icon2, R.id.icon3, R.id.icon4)
-            val arrayTemperature = listOf<Int>(R.id.temperature1, R.id.temperature2, R.id.temperature3, R.id.temperature4)
+            val arrayTemperature = listOf<Int>(
+                R.id.temperature1,
+                R.id.temperature2,
+                R.id.temperature3,
+                R.id.temperature4
+            )
 
             for (x in arrayData) {
-                views.setTextViewText(x, resultDays?.daily?.get(arrayData.indexOf(x))?.dt?.let { current(it.toLong()) })
+                views.setTextViewText(
+                    x,
+                    resultDays?.daily?.get(arrayData.indexOf(x))?.dt?.let { current(it.toLong()) })
             }
 
 //            for (x in arrayIcon) {
@@ -101,17 +115,39 @@ private fun load(
 //
 //            }
 
-
             for (x in arrayTemperature) {
-                views.setTextViewText(x, resultDays?.daily?.get(arrayTemperature.indexOf(x))?.temp?.dayTemp.toString())
+                views.setTextViewText(
+                    x,
+                    resultDays?.daily?.get(arrayTemperature.indexOf(x))?.temp?.dayTemp?.toInt().toString() + "/" + resultDays?.daily?.get(
+                        arrayTemperature.indexOf(x)
+                    )?.temp?.nightTemp?.toInt().toString()
+                )
             }
 
             //TODO resultDays вставлять уже во вьюхи лайаута
 
             // Instruct the widget manager to update the widget
+            val intent = Intent(context, WeatherWidget::class.java)
+            intent.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
+
+            val ids =
+                appWidgetManager.getAppWidgetIds(ComponentName(context, WeatherWidget::class.java))
+
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids)
+
+            val updateIntent = PendingIntent.getBroadcast(
+                context,
+                appWidgetId,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+            views.setOnClickPendingIntent(R.id.refresh_btn, updateIntent)
+
+            // Instruct the widget manager to update the widget
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
     }
+
 
 }
 
